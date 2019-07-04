bac98b6428a775536c2cd3d2ae7bb1aefa546c98
manaus inkul <manaus@digio.co.th>
2019-07-04 15:20:24 +0700

    no message

---
+++ config/config.js
@@ -0,0 +1,11 @@
+'use strict'
+var configParams = {
+  serviceAPIPort: 'https://dev-villa.digio.co.th:8448/digipayV2',
+  token: 'sRyHHtCFyoBGc2fwR0PN50wgsZg5mW1OJ/ACCTWGa7Of9JlVk43vtRkiQbGKWCGfM4uPAerxA3HwOknvlZ7ttP7ClCEo0ovf3DsTt2DHJ2SYWcx93Tp04n4UmNzjikZb6alQ8lOYrLtXtJClS9HZ9gdB04t89/1O/w1cDnyilFU=',
+  replyMessage: 'https://api.line.me/v2/bot/message/reply',
+  profile: 'https://api.line.me/v2/profile',
+  ChannelId: '1578415113',
+  ChannelSecret: '0ab2f0371cb4913f0610af259b098b28'
+}
+// EndopintSecret : b5383ad77f8216308c8515476a109de2bab27f180f7a88fcc50cd2481e44
+module.exports = configParams
---
+++ index.js
@@ -0,0 +1,27 @@
+// /**
+//  * @author Pamontep Panya
+//  * @email pamontep.p@gmail.com
+//  * @create date 2018-06-03 03:45:02
+//  * @modify date 2018-06-03 03:45:02
+//  * @desc A sample project of Node.js and Line API
+// */
+// const server = require('express')
+// const PORT = process.env.PORT || 4000
+// const request = require('request')
+// const bodyParser = require('body-parser')
+
+// server()
+//   .use(bodyParser.json())
+//   .use(bodyParser.urlencoded({extended: false}))
+//   .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
+//   // .post('/webHook', function (req, res) {
+//   //   let replyToken = req.body.events[0].replyToken
+//   //   let msg = req.body.events[0].message.text
+//   //   console.log(`Message token : ${replyToken}`)
+//   //   console.log(`Message from chat : ${msg}`)
+//   //   res.json({
+//   //     status: 200,
+//   //     message: `Webhook is working!`
+//   //   })
+//   // })
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`))
