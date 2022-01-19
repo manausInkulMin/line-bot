940c5734538d35176eaeecd036a0b1cd87537cc2
Manaus Inkul [Min] <manaus@reinvention.online>
2022-01-19 16:31:58 +0700

    no message

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 'use strict'
 var configParams = {
   serviceAPIPort: 'https://dev-villa.digio.co.th:8448/digipayV2',
-  token: 'sRyHHtCFyoBGc2fwR0PN50wgsZg5mW1OJ/ACCTWGa7Of9JlVk43vtRkiQbGKWCGfM4uPAerxA3HwOknvlZ7ttP7ClCEo0ovf3DsTt2DHJ2SYWcx93Tp04n4UmNzjikZb6alQ8lOYrLtXtJClS9HZ9gdB04t89/1O/w1cDnyilFU=',
+  token: 'TLeYuFS+BYD8g3lhkm/kATJcl1+FA7TupXT9ctHdiXoP6KU2cbfTlr/xPyUo4nau0rKAvMryOjK2bV4s5Pn6WjwJ/CCnQFv4VGdzcNWhNmUWpQk5ITpS5Vj/BjP9ioDjZqsno/HDEH44iSqmfSs/HgdB04t89/1O/w1cDnyilFU=',
   replyMessage: 'https://api.line.me/v2/bot/message/reply',
   profile: 'https://api.line.me/v2/profile',
   ChannelId: '1578415113',
-  ChannelSecret: '0ab2f0371cb4913f0610af259b098b28'
+  ChannelSecret: '0fcbfc562e3e51e4186b2b00817c2d49'
 }
 // EndopintSecret : b5383ad77f8216308c8515476a109de2bab27f180f7a88fcc50cd2481e44
 module.exports = configParams
